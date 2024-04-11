86eea90c1418c623169cb013455160827b13c962
jjfantini <jjman2811@gmail.com>
2024-04-11 10:51:35 -0400

    ♻️ refactor(suupabase): setting up server side auth, used supabase docs

--- frontend/utils/supabase/middleware.ts
+++ frontend/utils/supabase/middleware.ts
@@ -1,8 +1,7 @@
 import { createServerClient, type CookieOptions } from "@supabase/ssr";
-import { type NextRequest, NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
-export const createClient = (request: NextRequest) => {
-	// Create an unmodified response
+export async function updateSession(request: NextRequest) {
 	let response = NextResponse.next({
 		request: {
 			headers: request.headers,
@@ -18,7 +17,6 @@ export const createClient = (request: NextRequest) => {
 					return request.cookies.get(name)?.value;
 				},
 				set(name: string, value: string, options: CookieOptions) {
-					// If the cookie is updated, update the cookies for the request and response
 					request.cookies.set({
 						name,
 						value,
@@ -36,7 +34,6 @@ export const createClient = (request: NextRequest) => {
 					});
 				},
 				remove(name: string, options: CookieOptions) {
-					// If the cookie is removed, update the cookies for the request and response
 					request.cookies.set({
 						name,
 						value: "",
@@ -57,5 +54,7 @@ export const createClient = (request: NextRequest) => {
 		}
 	);
 
-	return { supabase, response };
-};
+	await supabase.auth.getUser();
+
+	return response;
+}
