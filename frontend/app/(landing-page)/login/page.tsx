2d5d9b8e6c023a263446b7cfe63dddcfb31293f5
jjfantini <jjman2811@gmail.com>
2024-04-15 18:45:39 +0200

    🐛 fix(login): made login background contianer fill the screen

--- frontend/app/(landing-page)/login/page.tsx
+++ frontend/app/(landing-page)/login/page.tsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
   const buttonClasses = "bg-foreground/10 dark:bg-foreground/20";
 
   return (
-    <div className="flex h-screen w-screen items-center justify-center bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500 p-2 sm:p-4 lg:p-8">
+    <div className=" fixed inset-0 flex h-screen w-screen items-center justify-center bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500 p-2 sm:p-4 lg:p-8">
       <div className="flex w-full max-w-sm flex-col gap-4 rounded-large bg-background/60 px-8 pb-10 pt-6 shadow-small backdrop-blur-md backdrop-saturate-150 dark:bg-default-100/50">
         <p className="pb-2 text-xl font-medium">Log In</p>
         <form className="flex flex-col gap-3">
