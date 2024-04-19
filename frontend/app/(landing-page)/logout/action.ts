bbae368f7452e1d929a668d364bfff9a4ecbac28
jjfantini <jjman2811@gmail.com>
2024-04-19 09:10:20 -0400

    ✨ feat(logout): logout button working in logout modal

--- frontend/app/(landing-page)/logout/action.ts
+++ frontend/app/(landing-page)/logout/action.ts
@@ -1,15 +1,19 @@
-'use server'
+"use server";
 import { createClient } from "@/utils/supabase/server";
-import { headers } from "next/headers";
+import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
-export const signOut = async (formData: FormData) => {
-    const supabase = createClient()
-    const { data, error } = await supabase.auth.getUser()
+export const signOut = async (scope: "global" | "local" | "others") => {
+	const supabase = createClient();
+	const { data, error } = await supabase.auth.getUser();
 
-    if (error || !data?.user) {
-        console.log("no user logged in")
-        redirect('/login')
-    }
-    const { error: signOutError } = await supabase.auth.signOut()
+	if (error || !data?.user) {
+		console.log("no user logged in");
+		redirect("/login");
+	}
+	if (data?.user) {
+		const { error: signOutError } = await supabase.auth.signOut({ scope });
+		revalidatePath("/dashboard", "layout");
+		redirect("/");
+	}
 };
--- frontend/components/(landing-page)/logout/LogoutModal.tsx
+++ frontend/components/(landing-page)/logout/LogoutModal.tsx
@@ -1,4 +1,5 @@
 "use client";
+import LogoutButton from "@/components/(landing-page)/logout/LogoutButton";
 import { Button } from "@nextui-org/button";
 import {
 	Modal,
@@ -37,32 +38,10 @@ export default function LogoutModal({
 					{(onClose) => (
 						<>
 							<ModalHeader className="flex flex-col gap-1">
-								Modal Title
+								Powering Down...
 							</ModalHeader>
 							<ModalBody>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit. Nullam pulvinar risus non
-									risus hendrerit venenatis. Pellentesque sit
-									amet hendrerit risus, sed porttitor quam.
-								</p>
-								<p>
-									Lorem ipsum dolor sit amet, consectetur
-									adipiscing elit. Nullam pulvinar risus non
-									risus hendrerit venenatis. Pellentesque sit
-									amet hendrerit risus, sed porttitor quam.
-								</p>
-								<p>
-									Magna exercitation reprehenderit magna aute
-									tempor cupidatat consequat elit dolor
-									adipisicing. Mollit dolor eiusmod sunt ex
-									incididunt cillum quis. Velit duis sit
-									officia eiusmod Lorem aliqua enim laboris do
-									dolor eiusmod. Et mollit incididunt nisi
-									consectetur esse laborum eiusmod pariatur
-									proident Lorem eiusmod et. Culpa deserunt
-									nostrud ad veniam.
-								</p>
+								<h1>You are about to log out.</h1>
 							</ModalBody>
 							<ModalFooter>
 								<Button
@@ -72,9 +51,7 @@ export default function LogoutModal({
 								>
 									Close
 								</Button>
-								<Button color="primary" onPress={onClose}>
-									Action
-								</Button>
+								<LogoutButton />
 							</ModalFooter>
 						</>
 					)}
