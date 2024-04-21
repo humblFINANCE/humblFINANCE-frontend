93bfb3699b4bdc0b1caf7e2aae70a41cb47f1b0c
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-04-22 03:51:33 +0700

    🚧 WIP(auth): auto register new user while login
    not tested yet due the emailing rate limit, this method is the simplest
    way to achieve how we know if the user already registeret at database
    
    if this method not work there is some other solution like adding custom
    function
    
    Closes #4

--- frontend/app/(landing-page)/login/action.ts
+++ frontend/app/(landing-page)/login/action.ts
@@ -4,16 +4,37 @@ import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 
 export const signIn = async (formData: FormData) => {
+	const origin = headers().get("origin");
 	const email = formData.get("email") as string;
 	const password = formData.get("password") as string;
-	const supabase = createClient();
-	
-	const { error } = await supabase.auth.signInWithPassword({
+	const supabase = createClient()
+
+	const { data, error: checkUserError } = await supabase.auth.signUp({
 		email,
 		password,
+		options: {
+			emailRedirectTo: `${origin}/auth/confirm`,
+		},
 	});
 
+	if (checkUserError) {
+		console.log('error heck user', checkUserError)
+		return redirect("/login?message=could not authenticate user");
+	}
+
+	const userAlreadyRegistered = data.user && data.user.identities && data.user.identities.length === 0
+	if (!userAlreadyRegistered) {
+		//TODO: add emai confirmation page
+		return redirect("/email-confirmation");
+	}
+
+	const { error } = await supabase.auth.signInWithPassword({
+		email,
+		password
+	})
+
 	if (error) {
+		console.log('error login', error)
 		return redirect("/login?message=could not authenticate user");
 	}
 
