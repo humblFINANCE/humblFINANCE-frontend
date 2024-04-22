1a7566d1d04b9ec6539bddd5474ff41cbc879303
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-04-23 03:10:34 +0700

    style(login): add button & modal login with magic link and phone

--- frontend/app/(landing-page)/login/page.tsx
+++ frontend/app/(landing-page)/login/page.tsx
@@ -12,10 +12,14 @@ import { useRouter } from 'next/navigation'
 import { signIn } from './action'
 import { createClient } from '@/utils/supabase/client'
 import React, { useCallback } from "react";
+import { useDisclosure } from "@nextui-org/modal";
+import PasswordLessLoginModal from "@/components/(landing-page)/login/PasswordlessModal";
 
 export default function LoginPage() {
   const router = useRouter()
   const [isVisible, setIsVisible] = React.useState(false);
+  const [passwordLessModalType, setPasswordLessModalType ] = React.useState<"magicLink" | "phoneNumber">("magicLink")
+  const { isOpen, onOpenChange, onOpen } = useDisclosure()
 
   const toggleVisibility = () => setIsVisible(!isVisible);
 
@@ -26,7 +30,7 @@ export default function LoginPage() {
 
   const buttonClasses = "bg-foreground/10 dark:bg-foreground/20";
 
-  const handleLoginWithOauth = useCallback( (provider: "google" | "github" | "twitter" | "discord") => {
+  const handleLoginWithOauth = useCallback((provider: "google" | "github" | "twitter" | "discord") => {
     return async function() {
       const supabase = createClient()
       const { error } = await supabase.auth.signInWithOAuth({
@@ -36,12 +40,20 @@ export default function LoginPage() {
         }
       })
 
+
       if (error) {
         router.replace("/login?message=" + "menyala")
       }
     }
   }, [router])
 
+  const handlePasswordLessLogin = useCallback((type: "magicLink" | "phoneNumber") => {
+    return function() {
+      setPasswordLessModalType(type)
+      onOpen()
+    }
+
+  }, [onOpen])
 
   return (
     <div className=" fixed inset-0 flex h-screen w-screen items-center justify-center bg-gradient-to-br from-rose-400 via-fuchsia-500 to-indigo-500 p-2 sm:p-4 lg:p-8">
@@ -110,12 +122,25 @@ export default function LoginPage() {
             Continue with Github
           </Button>
           <Button type="submit" onClick={handleLoginWithOauth('twitter')} className={buttonClasses} startContent={<Icon icon="fe:twitter" width={24} />}>
-            Continue with Twitter 
+            Continue with Twitter
           </Button>
           <Button type="submit" onClick={handleLoginWithOauth('discord')} className={buttonClasses} startContent={<Icon icon="ic:baseline-discord" width={24} />}>
-            Continue with Discord 
+            Continue with Discord
           </Button>
         </form>
+        <div className="flex items-center gap-4 py-2">
+          <Divider className="flex-1" />
+          <p className="shrink-0 text-tiny text-default-500">OR</p>
+          <Divider className="flex-1" />
+        </div>
+        <div className="flex flex-col gap-2">
+          <Button onClick={handlePasswordLessLogin('magicLink')} className={buttonClasses} startContent={<Icon icon="fe:link" width={24} />}>
+            Continue with Link
+          </Button>
+          <Button type="submit" onClick={handlePasswordLessLogin('phoneNumber')} className={buttonClasses} startContent={<Icon icon="fe:phone" width={24} />}>
+            Continue with Phone
+          </Button>
+        </div>
         <p className="text-center text-small text-foreground/50">
           Need to create an account?&nbsp;
           <Link color="foreground" href="#" size="sm">
@@ -123,6 +148,8 @@ export default function LoginPage() {
           </Link>
         </p>
       </div>
+      <PasswordLessLoginModal type={passwordLessModalType} onOpenChange={onOpenChange} isOpen={isOpen} />
     </div>
   );
 }
+
