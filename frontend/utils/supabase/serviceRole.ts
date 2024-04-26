e8e88501e796d40bcda090edab07adb2a6b8cd97
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-04-27 06:01:07 +0700

    ♻️ refactor(login): change flow auto register user

---
+++ frontend/utils/supabase/serviceRole.ts
@@ -0,0 +1,13 @@
+import { createClient } from '@supabase/supabase-js'
+
+export const createServiceRoleClient = () => {
+
+	if (typeof window !== "undefined") {
+		throw new Error("you cannot initiate serviceRoleClient at browser")
+	}
+
+  return createClient(
+		process.env.NEXT_PUBLIC_SUPABASE_URL!,
+		process.env.SUPABASE_SERVICE_ROLE_KEY!
+	)
+}
