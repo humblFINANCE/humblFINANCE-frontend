d57c476ff86d1a588e984a509a8c40f7a8a8872b
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-06-24 15:24:51 +0700

    🎨 style(eslint): setup eslint

---
+++ frontend/eslint.config.mjs
@@ -0,0 +1,14 @@
+// eslint.config.js
+import js from '@eslint/js'
+
+export default [
+  js.configs.recommended,
+
+  {
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-undef': 'warn',
+    },
+  },
+]
+
