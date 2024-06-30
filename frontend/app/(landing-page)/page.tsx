1a68877662bf7722a0ec8e8db93face12d24cf6a
jenningsFantini <jenningsfantini+github@gmail.com>
2024-06-30 14:14:23 +0100

    ✨ feat(aceternity): WavyBackground on landing page working

--- frontend/app/(landing-page)/page.tsx
+++ frontend/app/(landing-page)/page.tsx
@@ -1,16 +1,19 @@
 import HumblFinanceHeading from '@/components/(landing-page)/HumblFinanceHeading'
 import { title } from '@/components/Primitives'
+import { WavyBackground } from '@/features/aceternity/WavyBackground'
 
 export default function HomePage() {
   return (
     <section className="flex flex-col items-center justify-center gap-1">
-      <HumblFinanceHeading />
-      <br />
-      <div className="inline-block w-full text-center">
-        <h2 className={title({ size: 'xs' })}>
-          a modern investing framework built for everyone
-        </h2>
-      </div>
+      <WavyBackground>
+        <HumblFinanceHeading />
+        <br />
+        <div className="inline-block w-full text-center">
+          <h2 className={title({ size: 'xs' })}>
+            a modern investing framework built for everyone
+          </h2>
+        </div>
+      </WavyBackground>
     </section>
   )
 }
