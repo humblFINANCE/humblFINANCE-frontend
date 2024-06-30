a1eb8481f7fcdff0df29634b5e58831b17458c69
jenningsFantini <jenningsfantini+github@gmail.com>
2024-06-30 14:38:53 +0100

    🚧 WIP(Sparkles): the UI doesnt look the same as the the web

--- frontend/app/(landing-page)/page.tsx
+++ frontend/app/(landing-page)/page.tsx
@@ -1,19 +1,8 @@
 import HumblFinanceHeading from '@/components/(landing-page)/HumblFinanceHeading'
 import { title } from '@/components/Primitives'
 import { WavyBackground } from '@/features/aceternity/WavyBackground'
+import { SparklesPreview } from '@/features/aceternity/SparklesPreview'
 
 export default function HomePage() {
-  return (
-    <section className="flex flex-col items-center justify-center gap-1">
-      <WavyBackground waveHeight={0.5}>
-        <HumblFinanceHeading />
-        <br />
-        <div className="inline-block w-full text-center">
-          <h2 className={title({ size: 'xs' })}>
-            a modern investing framework built for everyone
-          </h2>
-        </div>
-      </WavyBackground>
-    </section>
-  )
+  return <SparklesPreview />
 }
