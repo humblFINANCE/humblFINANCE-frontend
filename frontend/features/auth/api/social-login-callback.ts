8f9cd372c341c83da9d2f2542dd74997f62c1996
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-07-15 19:34:44 +0700

    🚧 wip(social-login-callback): revert all changes

--- frontend/features/auth/api/social-login-callback.ts
+++ frontend/features/auth/api/social-login-callback.ts
@@ -7,50 +7,27 @@ export async function GET(request: NextRequest) {
   const code = searchParams.get('code')
   const redirectTo = request.nextUrl.clone()
 
-  let host = (request.headers.get("host") as string) || request.nextUrl.clone().host
-  let protocol = /^localhost(:\d+)?$/.test(host) ? "http:" : "https:";
-
-  if (
-    request.headers.get("x-forwarded-host") &&
-    typeof request.headers.get("x-forwarded-host") === "string"
-  ) {
-    host = request.headers.get("x-forwarded-host") as string
+  const host = request.headers.get("host")
+  console.log("hostkumenyala", host)
+  if (host) {
+    redirectTo.host = request.headers.get("host") as string
   }
 
-  if (
-    request.headers.get("x-forwarded-proto") &&
-    typeof request.headers.get("x-forwarded-proto") === "string"
-  ) {
-    protocol = `${request.headers.get("x-forwarded-proto")}:`;
+  if (code) {
+    const { error } = await supabase.auth.exchangeCodeForSession(code!)
+    if (!error) {
+      const from = searchParams.get('from')
+      console.log(from)
+
+      redirectTo.searchParams.delete('code')
+      redirectTo.pathname = '/dashboard/home'
+      if (redirectTo.searchParams.get('from')) {
+        redirectTo.searchParams.delete('from')
+        redirectTo.pathname = '/' + from
+      }
+      return NextResponse.redirect(redirectTo)
+    }
   }
-
-  // if (code) {
-  //   const { error } = await supabase.auth.exchangeCodeForSession(code!)
-  //   if (!error) {
-  //     const from = searchParams.get('from')
-  //
-  //     redirectTo.searchParams.delete('code')
-  //     redirectTo.pathname = '/dashboard/home'
-  //     if (redirectTo.searchParams.get('from')) {
-  //       redirectTo.searchParams.delete('from')
-  //       redirectTo.pathname = '/' + from
-  //     }
-  //     // return NextResponse.redirect(redirectTo)
-  //     return NextResponse.json({
-  //       host,
-  //       nextHost: redirectTo.host,
-  //       protocol: redirectTo.protocol,
-  //       redirectTo: redirectTo.toString()
-  //     })
-  //   }
-  // }
-
-  return NextResponse.json({
-    host,
-    nextHost: redirectTo.host,
-    protocol: redirectTo.protocol,
-    redirectTo: redirectTo.toString()
-  })
 
   redirectTo.pathname = '/error'
   return NextResponse.redirect(redirectTo)
