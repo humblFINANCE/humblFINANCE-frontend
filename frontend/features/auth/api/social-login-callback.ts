b8d9f8f02f3f7bccf3b8e55c147eb4bf8d567504
Rizky Tanjung <kimsohyuncans@gmail.com>
2024-07-20 19:24:31 +0700

    catch error social login callback

--- frontend/features/auth/api/social-login-callback.ts
+++ frontend/features/auth/api/social-login-callback.ts
@@ -2,32 +2,38 @@ import { createClient } from '@/utils/supabase/server'
 import { NextResponse, type NextRequest } from 'next/server'
 
 export async function GET(request: NextRequest) {
-  const supabase = createClient()
-  const { searchParams } = new URL(request.url)
-  const code = searchParams.get('code')
-  const redirectTo = request.nextUrl.clone()
+  try {
+    const supabase = createClient()
+    const { searchParams } = new URL(request.url)
+    const code = searchParams.get('code')
+    const redirectTo = request.nextUrl.clone()
 
-  const host = request.headers.get('host')
-  if (host) {
-    redirectTo.host = request.headers.get('host') as string
-  }
-
-  if (code) {
-    const { error } = await supabase.auth.exchangeCodeForSession(code!)
-    if (!error) {
-      const from = searchParams.get('from')
-      console.log(from)
-
-      redirectTo.searchParams.delete('code')
-      redirectTo.pathname = '/dashboard/home'
-      if (redirectTo.searchParams.get('from')) {
-        redirectTo.searchParams.delete('from')
-        redirectTo.pathname = '/' + from
-      }
-      return NextResponse.redirect(redirectTo)
+    const host = request.headers.get('host')
+    if (host) {
+      redirectTo.host = request.headers.get('host') as string
     }
-  }
 
-  redirectTo.pathname = '/error'
-  return NextResponse.redirect(redirectTo)
+    if (code) {
+      const { error } = await supabase.auth.exchangeCodeForSession(code!)
+      if (!error) {
+        const from = searchParams.get('from')
+        console.log(from)
+
+        redirectTo.searchParams.delete('code')
+        redirectTo.pathname = '/dashboard/home'
+        if (redirectTo.searchParams.get('from')) {
+          redirectTo.searchParams.delete('from')
+          redirectTo.pathname = '/' + from
+        }
+        return NextResponse.redirect(redirectTo)
+      }
+    }
+
+    redirectTo.pathname = '/error'
+    return NextResponse.redirect(redirectTo)
+  } catch (e: any) {
+    return NextResponse.json({
+      message: e?.message,
+    })
+  }
 }
