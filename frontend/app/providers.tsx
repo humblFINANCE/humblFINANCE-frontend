69371025638048d07ab1851b0294a2cbc46f22d9
Sundus <pcinta0@gmail.com>
2024-08-05 22:17:07 +0700

    fix: remove toast provider from provider.tsx

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -5,8 +5,6 @@ import { ThemeProvider as NextThemesProvider } from 'next-themes'
 import { ThemeProviderProps } from 'next-themes/dist/types'
 import { useRouter } from 'next/navigation'
 import * as React from 'react'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
 
 export function Providers({ children, ...themeProps }: ThemeProviderProps) {
   const router = useRouter()
@@ -14,7 +12,6 @@ export function Providers({ children, ...themeProps }: ThemeProviderProps) {
   return (
     <NextUIProvider navigate={router.push}>
       <NextThemesProvider {...themeProps}>{children}</NextThemesProvider>
-      <ToastContainer />
     </NextUIProvider>
   )
 }
