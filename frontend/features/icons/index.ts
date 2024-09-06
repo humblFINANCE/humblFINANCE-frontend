a5cac3edc218585c2a2148259de72ec4734e0930
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-06 13:00:19 +0100

    ♻️ refactor(icons): moved icons into features

---
+++ frontend/features/icons/index.ts
@@ -0,0 +1,10 @@
+export { MoonFilledIcon } from './MoonFilledIcon'
+export { SunFilledIcon } from './SunFilledIcon'
+export { HeartFilledIcon } from './HeartFilledIcon'
+export { SearchIcon } from './SearchIcon'
+
+// Assuming these are the Brands icons, adjust as necessary
+export { HumblPortoflio } from './HumblPortoflio'
+export { HumblCompass } from './HumblCompass'
+export { HumblChannel } from './HumblChannel'
+export { HumblAlerts } from './HumblAlerts'
