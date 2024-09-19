663498dbfe6d15868a916ee01a92ad3b905cce90
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-19 17:01:06 -0400

    🚧 WIP(MacbookScroll): macbook component on the dashboard

--- frontend/app/(landing-page)/page.tsx
+++ frontend/app/(landing-page)/page.tsx
@@ -1,6 +1,7 @@
 import HumblFinanceHeading from '@/components/(landing-page)/HumblFinanceHeading'
 import { title } from '@/components/Primitives'
-
+import { MacbookScroll } from '@/components/(landing-page)/MacbookScroll'
+import Image from 'next/image'
 export default function HomePage() {
   return (
     <section className="flex flex-col items-center justify-center gap-1">
@@ -10,6 +11,18 @@ export default function HomePage() {
         <h2 className={title({ size: 'xs' })}>
           a modern investing framework built for everyone
         </h2>
+
+        <MacbookScroll
+          src="/humblFINANCE-dashboard.png"
+          // badge={
+          //   <Image
+          //     src="/humblfinance.svg"
+          //     alt="HumblFinance Logo"
+          //     width={32}
+          //     height={32}
+          //   />
+          // }
+        />
       </div>
     </section>
   )
