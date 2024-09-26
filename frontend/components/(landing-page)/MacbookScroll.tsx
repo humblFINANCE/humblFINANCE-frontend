19cbf4a2ced6fa5ea53a7ecb4a603665b24fdb53
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-26 18:51:41 -0400

    🐛 fix(MAcbookScroll): fixed spacing on different screen sizes

--- frontend/components/(landing-page)/MacbookScroll.tsx
+++ frontend/components/(landing-page)/MacbookScroll.tsx
@@ -69,14 +69,14 @@ export const MacbookScroll = ({
   return (
     <div
       ref={ref}
-      className="min-h-[150vh] sm:min-h-[170vh] flex flex-col items-center py-0 md:py-40 justify-start flex-shrink-0 [perspective:800px] transform md:scale-110 scale-[0.6] sm:scale-75 -mt-20 sm:-mt-10 md:mt-0 relative"
+      className="min-h-[150vh] sm:min-h-[140vh] flex flex-col items-center py-0 md:py-40 justify-start flex-shrink-0 [perspective:800px] transform md:scale-110 scale-[0.6] sm:scale-75 -mt-20 sm:-mt-10 md:mt-0 relative"
     >
       <motion.h2
         style={{
           translateY: textTransform,
           opacity: textOpacity,
         }}
-        className="dark:text-white text-neutral-800 text-3xl font-bold mb-5 sm:mb-10 text-center"
+        className="dark:text-white text-neutral-800 text-3xl font-bold mb-10 text-center"
       >
         {title || <h3>financial freedom starts here</h3>}
       </motion.h2>
