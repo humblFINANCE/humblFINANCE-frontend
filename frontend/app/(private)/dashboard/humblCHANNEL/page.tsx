2f55d73753d2a01a8e4d280a1b5658258da11ed1
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-27 14:16:18 -0400

    🐛 fix(humblCHANNEL/COMPASS): added a coming soon basic page

--- frontend/app/(private)/dashboard/humblCHANNEL/page.tsx
+++ frontend/app/(private)/dashboard/humblCHANNEL/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const humblCHANNEL = () => {
   return (
     <div>
-      <h1>humblCHANNEL</h1>
+      <h1>humblCHANNEL is being built...it should be ready within 1 week</h1>
     </div>
   )
 }
--- frontend/app/(private)/dashboard/humblCOMPASS/page.tsx
+++ frontend/app/(private)/dashboard/humblCOMPASS/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const humblCOMPASS = () => {
   return (
     <div>
-      <h1>humblCOMPASS</h1>
+      <h1>humblCOMPASS is being built...it should be ready within 2 weeks</h1>
     </div>
   )
 }
