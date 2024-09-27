6ce72d2a438e558199277c46d0dc3ab41d438dfe
jenningsFantini <jenningsfantini+github@gmail.com>
2024-09-27 17:44:57 -0400

    🚧 WIP(TradingView): removes the sharp corners, need to remove the border still

--- frontend/features/dashboard/components/TickerTape.tsx
+++ frontend/features/dashboard/components/TickerTape.tsx
@@ -1,44 +1,58 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react'
 
 function TickerTape() {
-    const containerRef: any = useRef(null);
+  const containerRef: any = useRef(null)
 
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js";
-        script.async = true;
-        script.innerHTML = JSON.stringify({
-            symbols: [
-                { description: "", proName: "TURQUOISE:SHELL" },
-                { description: "", proName: "TURQUOISE:BARCL" },
-                { description: "", proName: "TURQUOISE:RRL" },
-                { description: "", proName: "TURQUOISE:BPL" },
-                { description: "", proName: "TURQUOISE:BTL" },
-                { description: "", proName: "TURQUOISE:TSCOL" },
-                { description: "", proName: "TURQUOISE:BATSL" },
-                { description: "", proName: "TURQUOISE:ULVRL" }
-            ],
-            showSymbolLogo: false,
-            colorTheme: "dark",
-            isTransparent: false,
-            displayMode: "compact",
-            locale: "en"
-        });
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-ticker-tape.js'
+    script.async = true
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        { description: '', proName: 'NASDAQ:AAPL' },
+        { description: '', proName: 'NASDAQ:NVDA' },
+        { description: '', proName: 'NASDAQ:TSLA' },
+        { description: '', proName: 'NASDAQ:AMZN' },
+        { description: '', proName: 'NASDAQ:META' },
+        { description: '', proName: 'NASDAQ:MSFT' },
+        { description: '', proName: 'NASDAQ:GOOGL' },
+        { description: '', proName: 'NASDAQ:AMD' },
+        { description: '', proName: 'NYSE:SNAP' },
+        { description: '', proName: 'NASDAQ:INTC' },
+        { description: '', proName: 'NASDAQ:SMCI' },
+        { description: '', proName: 'NYSE:TSM' },
+        { description: '', proName: 'NYSE:XOM' },
+        { description: '', proName: 'NYSE:DIS' },
+        { description: '', proName: 'NASDAQ:PYPL' },
+        { description: '', proName: 'NYSE:MMM' },
+      ],
+      showSymbolLogo: false,
+      colorTheme: 'dark',
+      isTransparent: false,
+      displayMode: 'compact',
+      locale: 'en',
+    })
 
-        if (containerRef.current) {
-            containerRef.current.appendChild(script);
-        }
+    if (containerRef.current) {
+      containerRef.current.appendChild(script)
+    }
 
-        return () => {
-            if (containerRef.current) {
-                containerRef.current.removeChild(script);
-            }
-        };
-    }, []);
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeChild(script)
+      }
+    }
+  }, [])
 
-    return (
-        <div className="tradingview-widget-container" ref={containerRef} />
-    );
+  return (
+    <div className="rounded-lg overflow-hidden">
+      <div
+        className="tradingview-widget-container !rounded-lg !border-none"
+        ref={containerRef}
+      />
+    </div>
+  )
 }
 
-export default TickerTape;
+export default TickerTape
--- frontend/features/dashboard/components/TradingViewWidget.tsx
+++ frontend/features/dashboard/components/TradingViewWidget.tsx
@@ -1,67 +1,69 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef } from 'react'
 
 function TradingViewWidget() {
-    const containerRef: any = useRef(null);
+  const containerRef = useRef<HTMLDivElement>(null)
 
-    useEffect(() => {
-        const script = document.createElement("script");
-        script.src = "https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js";
-        script.async = true;
-        script.innerHTML = JSON.stringify({
-            symbols: [
-                ["Apple", "AAPL|1D"],
-                ["Nvidia", "NVDA|1D"],
-                ["Tesla", "TSLA|1D"]
-            ],
-            chartOnly: false,
-            width: "100%",
-            height: "100%",
-            locale: "en",
-            colorTheme: "dark",
-            autosize: true,
-            borderRadius: 20,
-            showVolume: false,
-            showMA: false,
-            hideDateRanges: false,
-            hideMarketStatus: false,
-            hideSymbolLogo: false,
-            scalePosition: "right",
-            scaleMode: "Normal",
-            fontFamily: "-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif",
-            fontSize: "10",
-            noTimeScale: false,
-            valuesTracking: "1",
-            changeMode: "price-and-percent",
-            chartType: "area",
-            maLineColor: "#2962FF",
-            maLineWidth: 1,
-            maLength: 9,
-            lineWidth: 2,
-            lineType: 0,
-            dateRanges: [
-                "1d|1",
-                "1m|30",
-                "3m|60",
-                "12m|1D",
-                "60m|1W",
-                "all|1M"
-            ]
-        });
+  useEffect(() => {
+    const script = document.createElement('script')
+    script.src =
+      'https://s3.tradingview.com/external-embedding/embed-widget-symbol-overview.js'
+    script.async = true
+    script.innerHTML = JSON.stringify({
+      symbols: [
+        ['S&P500', 'SPY|1D'],
+        ['Russell 2000', 'IWM|1D'],
+        ['Dow Jones', 'DIA|1D'],
+        ['NASDAQ 100', 'QQQ|1D'],
+      ],
+      chartOnly: false,
+      width: '100%',
+      height: '100%',
+      locale: 'en',
+      colorTheme: 'dark',
+      autosize: true,
+      borderRadius: 0, // Remove the borderRadius from the widget config
+      showVolume: false,
+      showMA: false,
+      hideDateRanges: false,
+      hideMarketStatus: false,
+      hideSymbolLogo: false,
+      scalePosition: 'right',
+      scaleMode: 'Normal',
+      fontFamily:
+        '-apple-system, BlinkMacSystemFont, Trebuchet MS, Roboto, Ubuntu, sans-serif',
+      fontSize: '10',
+      noTimeScale: false,
+      valuesTracking: '1',
+      changeMode: 'price-and-percent',
+      chartType: 'area',
+      maLineColor: '#2962FF',
+      maLineWidth: 1,
+      maLength: 9,
+      lineWidth: 2,
+      lineType: 0,
+      dateRanges: ['1d|1', '1m|30', '3m|60', '12m|1D', '60m|1W', 'all|1M'],
+      backgroundColor: 'rgba(0, 0, 0, 1)',
+    })
 
-        if (containerRef.current) {
-            containerRef.current.appendChild(script);
-        }
+    if (containerRef.current) {
+      containerRef.current.appendChild(script)
+    }
 
-        return () => {
-            if (containerRef.current) {
-                containerRef.current.removeChild(script);
-            }
-        };
-    }, []);
+    return () => {
+      if (containerRef.current) {
+        containerRef.current.removeChild(script)
+      }
+    }
+  }, [])
 
-    return (
-        <div className="tradingview-widget-container !rounded-md" ref={containerRef} />
-    );
+  return (
+    <div className="relative w-full h-full rounded-lg overflow-hidden">
+      <div
+        className="tradingview-widget-container absolute inset-0"
+        ref={containerRef}
+      />
+    </div>
+  )
 }
 
-export default TradingViewWidget;
+export default TradingViewWidget
