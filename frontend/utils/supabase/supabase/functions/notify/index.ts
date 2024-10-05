1d51c0d8a6b4bc45ddf879faefbb59ab79b18860
Sundus <pcinta0@gmail.com>
2024-10-06 04:31:32 +0700

    feat: supabase edge function

---
+++ frontend/utils/supabase/supabase/functions/notify/index.ts
@@ -0,0 +1,76 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+// Setup type definitions for built-in Supabase Runtime APIs
+import { serve } from 'https://deno.land/std@0.177.0/http/server.ts'
+import * as OneSignal from 'https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7'
+
+const _OnesignalAppId_ = Deno.env.get('ONESIGNAL_APP_ID')!
+const _OnesignalUserAuthKey_ = Deno.env.get('USER_AUTH_KEY')!
+const _OnesignalRestApiKey_ = Deno.env.get('ONESIGNAL_REST_API_KEY')!
+const configuration = OneSignal.createConfiguration({
+  userKey: _OnesignalUserAuthKey_,
+  appKey: _OnesignalRestApiKey_,
+})
+
+const onesignal = new OneSignal.DefaultApi(configuration)
+
+serve(async (req) => {
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', {
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'authorization, x-client-info, apikey, content-type',
+      },
+    })
+  }
+  try {
+    const { message, user_id } = await req.json()
+
+    // Build OneSignal notification object
+    const notification = new OneSignal.Notification()
+    notification.app_id = _OnesignalAppId_
+    notification.include_external_user_ids = [user_id]
+    notification.contents = {
+      en: message,
+    }
+    const onesignalApiRes = await onesignal.createNotification(notification)
+
+    return new Response(
+      JSON.stringify({ onesignalResponse: onesignalApiRes }),
+      {
+        headers: {
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers':
+            'authorization, x-client-info, apikey, content-type',
+        },
+      }
+    )
+  } catch (err) {
+    console.error('Failed to create OneSignal notification', err)
+    return new Response('Server error.', {
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers':
+          'authorization, x-client-info, apikey, content-type',
+      },
+      status: 400,
+    })
+  }
+})
+
+/* To invoke locally:
+
+  1. Run `supabase start` (see: https://supabase.com/docs/reference/cli/supabase-start)
+  2. Make an HTTP request:
+
+  curl -i --location --request POST 'http://127.0.0.1:54321/functions/v1/notify' \
+    --header 'Authorization: Bearer ' \
+    --header 'Content-Type: application/json' \
+    --data '{"name":"Functions"}'
+
+*/
